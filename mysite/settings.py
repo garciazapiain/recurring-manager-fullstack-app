64d856f7a1e2ad0012d659bdeb8207f72b566232
garciazapiain <juangarciazapiain@gmail.com>
2023-02-19 10:40:49 +0100

    removed dotenv

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,13 +12,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR, ".env"))
-SECRET_KEY = os.getenv("SECRET_KEY")
+# load_dotenv(os.path.join(BASE_DIR, ".env"))
+# SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # Quick-start development settings - unsuitable for production
