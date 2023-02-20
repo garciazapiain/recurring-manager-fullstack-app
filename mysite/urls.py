04433f5bdcc848460ae9c99dc9cc8838871dea8f
garciazapiain <juangarciazapiain@gmail.com>
2023-02-20 09:03:34 +0100

    experiment

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,7 +24,7 @@ router.register(r'products', views.ProductView, 'product')
 router.register(r'productcategories', views.ProductCategoryView, 'product')  
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.ProductView, name='product'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/products/add/', add_product, name='add_product'), 
