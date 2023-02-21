f58a1695bf2c41465b27e3d96589455783c73c08
garciazapiain <juangarciazapiain@gmail.com>
2023-02-21 07:01:42 +0100

    changes in views.py

--- product/views.py
+++ product/views.py
@@ -9,10 +9,52 @@ from .forms import ProductForm
 class ProductView(viewsets.ModelViewSet):  
     serializer_class = ProductSerializer   
     queryset = Product.objects.all()
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+
+    def as_view(self, actions=None, **kwargs):
+        actions = {
+            'get': 'list',
+            'post': 'create',
+            'put': 'update',
+            'patch': 'partial_update',
+            'delete': 'destroy',
+        }
+        return super().as_view(actions=actions, **kwargs)
 
 class ProductCategoryView(viewsets.ModelViewSet):  
     serializer_class = ProductCategorySerializer   
-    queryset = ProductCategory.objects.all() 
+    queryset = ProductCategory.objects.all()
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+
+    def as_view(self, actions=None, **kwargs):
+        actions = {
+            'get': 'list',
+            'post': 'create',
+            'put': 'update',
+            'patch': 'partial_update',
+            'delete': 'destroy',
+        }
+        return super().as_view(actions=actions, **kwargs) 
 
 def add_product(request):
         if request.method == 'POST':
