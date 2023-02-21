eb449d1c914cbfd78d5d19c6125bb0845a6aeb2b
garciazapiain <juangarciazapiain@gmail.com>
2023-02-21 07:27:47 +0100

    adjusted path for productviewset

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,7 +24,7 @@ router.register(r'products', views.ProductViewSet, 'product')
 router.register(r'productcategories', views.ProductCategoryView, 'product')  
 
 urlpatterns = [
-    path('', views.ProductViewSet.as_view(), name='product'),
+    path('', views.ProductViewSet.as_view({'get': 'list', 'post': 'create'}), name='product'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/products/add/', add_product, name='add_product'), 
