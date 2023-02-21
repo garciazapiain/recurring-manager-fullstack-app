417b5691fd034b660b8b37a97964f5edb66eba52
garciazapiain <juangarciazapiain@gmail.com>
2023-02-21 07:44:17 +0100

    changed to debug false

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x$53m^zm*)t4mskm301o$yezxz@#eoh-j)(qg=inpkulv153xw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
