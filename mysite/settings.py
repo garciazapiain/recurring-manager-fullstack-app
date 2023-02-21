a1cbe864a3021676f33476e574c934874de8d713
garciazapiain <juangarciazapiain@gmail.com>
2023-02-21 21:39:29 +0100

    static experiment

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,5 +147,7 @@ import dj_database_url
 DATABASES = {"default": dj_database_url.config()}
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend/build/static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
