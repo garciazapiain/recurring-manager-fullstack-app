1b58bfa7a4e28e6ed3f268c3ed36ba6838391b63
garciazapiain <juangarciazapiain@gmail.com>
2023-02-21 07:13:41 +0100

    modified product view

--- product/views.py
+++ product/views.py
@@ -5,8 +5,9 @@ from rest_framework import viewsets
 from .models import Product                 
 from .models import ProductCategory    
 from .forms import ProductForm
+from rest_framework.viewsets import ModelViewSet
 
-class ProductView(viewsets.ModelViewSet):  
+class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer   
     queryset = Product.objects.all()
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
@@ -20,6 +21,20 @@ class ProductView(viewsets.ModelViewSet):
         else:
             permission_classes = [AllowAny]
         return [permission() for permission in permission_classes]
+
+    def list(self, request):
+        # your code here
+        products = self.queryset
+        serializer = self.serializer_class(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        # your code here
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def as_view(self, actions=None, **kwargs):
         actions = {
@@ -66,8 +81,3 @@ def add_product(request):
             form = ProductForm()
 
         return render(request, 'add_product.html', {'form': form}) 
-
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Welcome to my recurring manager app!")
