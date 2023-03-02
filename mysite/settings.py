e8d4059d730ce115d0430cea24cc3fd4642453c1
garciazapiain <juangarciazapiain@gmail.com>
2023-03-02 19:25:27 +0100

    debug false

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x$53m^zm*)t4mskm301o$yezxz@#eoh-j)(qg=inpkulv153xw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','recurring-manager-app.herokuapp.com']
 
