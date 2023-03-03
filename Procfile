d68fe28eb59a9ba83e7d861b2982e937fea2d27e
garciazapiain <juangarciazapiain@gmail.com>
2023-03-03 14:57:13 +0100

    changed back

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
-web: sh -c 'cd frontend && npm install && npm start && cd .. && gunicorn mysite.wsgi --log-file -'
+web: gunicorn mysite.wsgi --log-file -
+release: python manage.py migrate
+frontend: cd frontend && npm install && npm run build
+static: cd frontend && serve -s build
--- mysite/settings.py
+++ mysite/settings.py
@@ -155,7 +155,9 @@ CORS_ORIGIN_WHITELIST = [
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'frontend', 'build'), ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
