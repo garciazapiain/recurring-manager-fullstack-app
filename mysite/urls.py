86cc775994ad52ae0669c19c04c059a101bee267
garciazapiain <juangarciazapiain@gmail.com>
2023-03-06 08:54:07 +0100

    urls.py experiment

--- mysite/urls.py
+++ mysite/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/products/add/', add_product, name='add_product'), 
-    path('api/csrf_token/', csrf_token, name='csrf_token'),
-    path('', views.home, name='home'),
+    path('api/csrf_token/', csrf_token, name='csrf_token'),  
+    path('<path:path>', TemplateView.as_view(template_name='index.html')),
 ]
