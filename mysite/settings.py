36977364038e5cd44bc47218c281da367b3319c3
garciazapiain <juangarciazapiain@gmail.com>
2023-03-10 20:10:03 +0100

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -156,7 +156,6 @@ CORS_ORIGIN_WHITELIST = [
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
     BASE_DIR / "frontend" / "build" / "static",
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
