523af0cb53567e10bde5bee17ed4a86416db8281
garciazapiain <juangarciazapiain@gmail.com>
2023-03-12 14:21:30 +0100

    new urls try

--- mysite/urls.py
+++ mysite/urls.py
@@ -27,10 +27,15 @@ router.register(r'products', views.ProductView, 'product')
 router.register(r'productcategories', views.ProductCategoryView, 'product')  
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/products/add/', add_product, name='add_product'), 
     path('api/csrf_token/', csrf_token, name='csrf_token'),  
     path('', views.home, name='home'),
-    # re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
+
+# Only add the re_path for non-admin URLs
+if not settings.DEBUG:  # Exclude in debug mode
+    urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html')),]
+
+# Add admin URLs
+urlpatterns += [path('admin/', admin.site.urls)]
