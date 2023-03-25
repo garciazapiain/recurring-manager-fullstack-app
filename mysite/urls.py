f88eb0d75320d3b8ddb95b0106ca5b95e46c9ba4
garciazapiain <juangarciazapiain@gmail.com>
2023-03-25 12:09:49 +0100

    again

--- mysite/urls.py
+++ mysite/urls.py
@@ -27,9 +27,9 @@ router.register(r'products', views.ProductView, 'product')
 router.register(r'productcategories', views.ProductCategoryView, 'product')  
 
 urlpatterns = [
-    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
     path('api/', include(router.urls)),
     path('api/products/add/', add_product, name='add_product'), 
     path('api/csrf_token/', csrf_token, name='csrf_token'),  
     path('admin/', admin.site.urls),
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
