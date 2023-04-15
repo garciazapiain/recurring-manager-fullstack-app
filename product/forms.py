0e9c567e2d8e63958ba15ca3d84d2ce2c3471cfb
garciazapiain <juangarciazapiain@gmail.com>
2023-04-15 10:41:42 +0200

    changes

--- product/forms.py
+++ product/forms.py
@@ -2,7 +2,11 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
-
     class Meta:
         model = Product
-        fields = ('title', 'category')
+        fields = ['title', 'category', 'unit', 'standard_size', 'use_days', 'current_inventory']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['author'].widget = forms.HiddenInput()
+        self.fields['author'].initial = self.instance.author_id if self.instance else self.request.user.id
