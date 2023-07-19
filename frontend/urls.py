1e9f400beb1f02eb75404b82a0d2f330355f86be
garciazapiain <juangarciazapiain@gmail.com>
2023-07-19 15:50:36 +0200

    changes for urls.py

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index, product_library
 
 urlpatterns = [
-    path('', index),
+    path('', index,name='index'),
     path('product-library/', product_library),
   
 ]
